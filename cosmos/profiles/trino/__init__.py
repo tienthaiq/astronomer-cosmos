81e14d1587dbba29e79b6115c95cb1a80279203d
Thai Quang Tien <tienthaiq@gmail.com>
2023-08-08 22:29:30 +0700

    Register profile

--- cosmos/profiles/trino/__init__.py
+++ cosmos/profiles/trino/__init__.py
@@ -4,10 +4,12 @@ from .base import TrinoBaseProfileMapping
 from .certificate import TrinoCertificateProfileMapping
 from .jwt import TrinoJWTProfileMapping
 from .ldap import TrinoLDAPProfileMapping
+from .oauth_client_credential import TrinoOAuthClientProfileMapping
 
 __all__ = [
     "TrinoBaseProfileMapping",
     "TrinoCertificateProfileMapping",
     "TrinoJWTProfileMapping",
     "TrinoLDAPProfileMapping",
+    "TrinoOAuthClientProfileMapping",
 ]
